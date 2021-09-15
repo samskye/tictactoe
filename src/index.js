128aa0fe79463725de85237f8f6ca8c521cb3d3a
samskye <shubhampatil550@gmail.com>
2021-09-15 16:37:51 +0530

    Init Commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+//dist folder is to be deployed to server if we have one, after npm run build
