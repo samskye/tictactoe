78763b6e464d71faeb925017fe4154d846c33bc6
samskye <shubhampatil550@gmail.com>
2021-09-16 08:06:14 +0530

    calculate winner and and lift up the state

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,25 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 /* need to use state management to keep track of the board */
-const Board = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isNext, setIsNext] = useState(false);
-  const handleSquareClick = position => {
-    if (board[position]) {
-      return;
-    }
-    setBoard(prev => {
-      return prev.map((square, pos) => {
-        if (pos === position) {
-          return isNext ? 'X' : 'O';
-        }
-        return square;
-      });
-    });
-
-    setIsNext(prev => !prev);
-  };
-
+const Board = ({ board, handleSquareClick }) => {
   const renderSquare = position => {
     return (
       <Square
